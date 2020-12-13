064888991916da9e4aecc87dcb219ad16582b157
Qadree Trimble <qadree_t@yahoo.com>
2020-12-12 16:34:30 -0800

    updating dynamics on page

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var promptQuestion = "What's the best comfort food? Tacos or Nachos?";
 var response = prompt(promptQuestion);
 console.log(promptQuestion);
 
-if (response === "Nachos") {
-    console.log("Exactly! High Five!)
-}else if(response === "Tacos")
+//if (response === "Nachos") {
+//    console.log("Exactly! High Five!)
+//}else if(response === "Tacos") {
+//    console.log("Wrong! Try Again!");
+//}else {}
