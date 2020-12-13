f4d2a49c8366f1235502fb1b4814b1ed8cf71ae9
Qadree Trimble <qadree_t@yahoo.com>
2020-12-12 16:09:34 -0800

    creating dynamics for webpage

---
+++ apps.js
@@ -0,0 +1,9 @@
+console.log("Hello Seattle");
+
+var promptQuestion = "What's the best comfort food? Tacos or Nachos?";
+var response = prompt(promptQuestion);
+console.log(promptQuestion);
+
+if (response === "Nachos") {
+    console.log("Exactly! High Five!)
+}else if(response === "Tacos")
