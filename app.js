bcadd97b90d62b7930bae74bb601989b0fc9bb9d
Qadree Trimble <qadree_t@yahoo.com>
2020-12-12 17:31:30 -0800

    updating for dynamic experience

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ console.log("Hello Seattle");
 
 var promptQuestion = "What's the best comfort food? Tacos or Nachos?";
 var response = prompt(promptQuestion);
-console.log(promptQuestion);
+console.log(response);
+var reflected;
+var color;
+var alertMessage;
 
-//if (response === "Nachos") {
-//    console.log("Exactly! High Five!)
-//}else if(response === "Tacos") {
-//    console.log("Wrong! Try Again!");
-//}else {}
+if (response === "Nachos") {
+    reflected = "Exactly! High Five!"
+}else if(response === "Tacos") {
+    console.log = (respone)
+    alertMessage = "Tacos are good but you can do better!"
+    color = "#FF0000";
+}else {
+    reflected = "Not real food!"
+    color = "#FF0000";
+}
+alert(reflected);
