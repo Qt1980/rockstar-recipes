41118674d481b6ae1bb5f60462629e18dbb9c38c
Qadree Trimble <qadree_t@yahoo.com>
2020-12-14 22:12:08 -0800

    updating app with functions

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 console.log("Hello Seattle");
 
-var promptQuestion = "What's the best comfort food? Tacos or Nachos?";
-var response = prompt(promptQuestion);
-console.log(response);
-var reflected;
-
-var alertMessage = "";
-var color;"#FF0000"
-
-if (response === "Nachos") {
-    alertMessage = "Exactly! High Five!";
-    color = "#0000ff";
-}else if(response === "Tacos") {
-    alertMessage = "Tacos are good but you can do better!"
-    color = "#FF0000";
-}else {
-    reflected = "Not real food!"
-    alert(reflected)
+function realtalk(){
+    var promptQuestion = "What's the best comfort food? Tacos or Nachos?";
+    var response = prompt(promptQuestion);
+    nachosortacos(response);
 }
-var container = document.getElementById("reflected")
-container.innerText = alertMessage
-container.style.color = color
+
+function nachosortacos(response){
+    var alertMessage = "";
+    var color = "";
+    if (response === "Nachos"){
+        alertMessage = "Exactly! High Five!";
+        color = "#0000ff";
+    }     
+    else if(response === "Tacos"){
+        alertMessage = "Tacos are good but you can do better!";
+        color = "#FF0000";
+    }       
+    else {
+        alertMessage = "Not real food!";
+    }
+    
+    var container = document.getElementById("reflected")
+    container.innerText = alertMessage
+    container.style.color = color
+    alert(alertMessage);
+}
+realtalk();
+
