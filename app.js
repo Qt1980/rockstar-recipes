0191b0bbed757537ce18fe5b4974e233cc177166
Adam DuQuette <duquetteadam@gmail.com>
2020-12-15 20:38:56 -0800

    add code review

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+
+
+
 console.log('Hello Seattle')
 
 var today = new Date();
@@ -14,35 +18,51 @@ if (hourNow >18) {
 
 document.write(greeting)
 
+// This function will prompt the user with a question
 function realtalk(){
+    // Declare a variable to hold the prompt box question
     var promptQuestion = "What's the best comfort food? Tacos or Nachos?";
+    
+    // Prompt box is called
     var response = prompt(promptQuestion);
+
+    // Calling another function within a function
     nachosortacos(response);
+
+    return response;
 }
 
+
 function nachosortacos(response){
+    // Declare alertMessage variable
     var alertMessage = "";
+
     if (response === "Nachos"){
-        alertMessage = "Exactly! High Five!";
-    }     
+        alertMessage = "Exactly! High Five!"; //if "Nachos"
+    }    
     else if(response === "Tacos"){
-        alertMessage = "Tacos are good but you can do better!";
+        alertMessage = "Tacos are good but you can do better!"; //if "Tacos"
     }       
     else {
-        alertMessage = "Not real food!";
+        alertMessage = "Not real food!"; //catch-all for everything else
         color = "White";
     }
     
-    var container = document.getElementById("reflected")
-    container.innerText = alertMessage
-    container.style.color = color
+    // Declare new variable, assign the value returned from
+    // getElementById("reflected")
+    var container = document.getElementById("reflected");
+
+    // Populate the container's innerText with the value
+    // of alertMessage
+    container.innerText = alertMessage;
+
+
+    container.style.color = color;
     alert(alertMessage);
 }
-realtalk();
 
-var color;
-
-function changebackgroundcolor(answer){
+function changebackgroundcolor(answer) {
+    var color;
 
     if (answer === "Nachos"){
         color = "silver";
@@ -50,5 +70,16 @@ function changebackgroundcolor(answer){
     else if(answer === "Tacos"){
         color = "Yellow";
     }
+
+    // dot-chaining
     document.getElementById("content").style.backgroundColor = color;
+
+    // var element = document.getElementById("content");
+    // var elementStyle = element.style;
+    // var elementBckGrdColor = elementStyle.backgroundColor;
+    // elementBckGrdColor = color;
 }
+
+var response;
+response = realtalk();
+changebackgroundcolor(response);
